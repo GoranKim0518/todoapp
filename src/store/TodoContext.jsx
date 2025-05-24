33eb24f18f7bf9891684824a0451b6e50cf5e6ed
GoranKim0518 <sungmin051830@gmail.com>
2025-05-24 19:03:06 +0900

    feat(add todo): add function that makes new todo bracket

--- src/store/TodoContext.jsx
+++ src/store/TodoContext.jsx
@@ -1,9 +1,21 @@
-import { createContext } from "react";
+import { useState, createContext } from "react";
 
 export const TodoContext = createContext();
 
 export function TodoProvider({ children }) {
-    const myValue = '';
+    const [todos, setTodos] = useState([]);
+
+     function addTodo(insertedText){
+        const newTodo = {
+            id: todos.length + 1,
+            content: insertedText,
+            isCompleted: false,
+        };
+        setTodos([...todos, newTodo]);
+    };
+    
+    const myValue = { todos, addTodo };
+
     return(
         <TodoContext.Provider value={myValue}>{children}</TodoContext.Provider>
     );
