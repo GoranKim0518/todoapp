173ad2f148a622252f347ea96f07c1608e0c9efe
GoranKim0518 <sungmin051830@gmail.com>
2025-05-24 17:30:29 +0900

    feat(todolist): add ToDoList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { TodoProvider } from './store/TodoContext.jsx';
 import './App.css'
+import Header from '../src/components/Header.jsx'
+import InsertTodo from '../src/components/InsertTodo.jsx'
+import ToDoList from './components/ToDoList.jsx';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <TodoProvider>
+      <Header />
+      <InsertTodo />
+      <ToDoList />
+    </TodoProvider>
   )
 }
 
