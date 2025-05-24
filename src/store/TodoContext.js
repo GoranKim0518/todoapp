3452c3648fcf8adaa5fd4330a14ac95ef2451957
GoranKim0518 <sungmin051830@gmail.com>
2025-05-24 19:08:57 +0900

    remove(two files): remove AdddTodo.jsx, TodoContext.js

--- src/store/TodoContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-const TodoContext = createContext();
-
-export default function TodoProvider({ children }) {
-    return(
-        <TodoContext.Provider>{children}</TodoContext.Provider>
-    );
-}
